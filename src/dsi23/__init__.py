981370e9c03248495e748a5550adff64d68a03a4
NCIR <ncir@novonordisk.com>
2024-06-17 08:41:29 +0200

    we do not need novopy

--- src/dsi23/__init__.py
+++ src/dsi23/__init__.py
@@ -3,6 +3,6 @@
 __version__ = "0.1.1"
 
 from pathlib import Path
-from novopy.metrics import metrics_decorators
+#from novopy.metrics import metrics_decorators
 
 dsi23_ROOT_DIR = Path(__file__).parent
