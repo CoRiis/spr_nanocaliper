7e48742723ebfa6e8d5224ce5c3dd48c6ab25793
NCIR <ncir@novonordisk.com>
2025-11-28 13:01:01 +0100

    clean-up

--- tests/unit/test_utils.py
+++
@@ -1,9 +0,0 @@
-"""Test functionality of the utility functions."""
-
-import pytest  # noqa: F401
-
-
-def test_utils():
-    """Test utils is intended just as a first test automatic constructed."""
-
-    assert True, "Always True - only included as initial."
